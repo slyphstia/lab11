e934b48c371555a2628dc564870811a255e42807
slyphstia <apiwit_boonyarit@cmu.ac.th>
2023-01-03 16:55:48 +0700

    complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,35 @@
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    double x,m;
+    double i=0;
+    while(getline(source,textline)){
+        x=x+stod(textline);
+        m=pow(stod(textline),2)+m;
+
+        i++;
+
+
+    }
+  double mean,sd ;
+  mean = (1/i)*x;
+  sd = sqrt((1/i)*m-pow(mean,2)) ;
+
+
+    cout << "Number of data = " << i <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<< mean <<"\n";
+    cout << "Standard deviation = "<< sd ;
+
+}
